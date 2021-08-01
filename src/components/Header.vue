f0b4e93d1bc8590ca322bc3a8534d16f9f630942
alexeiantonov91 <alexeiantonov91@gmail.com>
2021-08-01 07:20:39 +0300

    header update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,53 +3,58 @@
     <header
       :class="{ 'headroom--unpinned': scrolled }"
       v-on="handleScroll"
-      class="headroom header"
-    >
+      class="headroom header">
       <div class="wrap">
-        <a href="#" class="header-banner">
-          <img src="@/assets/images/logo.png" alt="logo" >
-        </a>
         
         <ul class="site-nav site-nav-first">
           <li><a href="">POPULAR</a></li>
           <li><a href="">NOW PLAYING</a></li>
           <li><a href="">UPCOMING</a></li>
-        </ul>
-        
-        <ul class="site-nav site-nav-second">
+
+          <li class="header-banner">
+            <a href="#" class="header-logo">
+              <img src="@/assets/images/logo.png" alt="logo" >
+            </a>
+          </li>
+          
           <li><a href="">WATCHLIST</a></li>
           <li><a href="">TV</a></li>
           <li><a href="">MOVIES</a></li>
         </ul>
+        
       </div>
       
     </header>
 
     <section class="search-wrap">
-      <input
-        v-model="searchInput"
-        @keyup.enter="onSubmit"
-        type="text"
-        class="
-          appearance-none
-          min-w-0
-          w-full
-          bg-white
-          border border-gray-300
-          rounded-md
-          shadow-sm
-          py-2
-          px-4
-          text-base text-gray-900
-          placeholder-gray-500
-          focus:outline-none
-          focus:ring-indigo-500
-          focus:border-indigo-500
-          focus:placeholder-gray-400
-        "
-      />
-      <p>{{ searchInput }}</p>
+      <div class="wrap">
+        <input
+          placeholder="Search"
+          v-model="searchInput"
+          @keyup.enter="onSubmit"
+          type="text"
+          class="
+            appearance-none
+            min-w-0
+            w-full
+            bg-white
+            border border-gray-300
+            rounded-md
+            shadow-sm
+            py-2
+            px-4
+            text-base text-gray-900
+            placeholder-gray-500
+            focus:outline-none
+            focus:ring-indigo-500
+            focus:border-indigo-500
+            focus:placeholder-gray-400
+          "
+        />
+        <p>{{ searchInput }}</p>
+      </div>
     </section>
+    
   </div>
 </template>
 
